96c6d875c6cf5073b47bad3a9f2e89b74ab81890
Zhixin Orange <120090222@link.cuhk.edu.cn>
2024-10-11 10:53:59 -0400

    Update the logics for multiple tests

--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -2,6 +2,7 @@ package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,9 +16,18 @@ public class AndrewWebServicesTest {
     @Before
     public void setUp() {
         // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+//        database = new Database(); // We probably don't want to access our real database...
+//        recommender = new RecSys();
+//        promoService = new PromoService();
+
+
+        database = mock(Database.class);
+        when(database.getPassword("Scotty")).thenReturn(17214);
+
+        recommender = mock(RecSys.class);
+        when(recommender.getRecommendation("Scotty")).thenReturn("Animal House");
+
+        promoService = mock(PromoService.class);
 
         andrewWebService = new AndrewWebServices(database, recommender, promoService);
     }
@@ -25,24 +35,33 @@ public class AndrewWebServicesTest {
     @Test
     public void testLogIn() {
         // This is taking way too long to test
-        assertTrue(andrewWebService.logIn("Scotty", 17214));
+        // Using fake to test login
+        boolean loggedIn = andrewWebService.logIn("Scotty", 17214);
+        assertTrue(loggedIn);
     }
 
     @Test
     public void testGetRecommendation() {
         // This is taking way too long to test
-        assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        // Using stub to test recommendation
+        String recommendation = andrewWebService.getRecommendation("Scotty");
+        assertEquals("Animal House", recommendation);
     }
 
     @Test
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        // Using mock to test sendEmail
+        andrewWebService.sendPromoEmail("zhixin@cmu.edu");
+        verify(promoService).mailTo("zhixin@cmu.edu");
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.logIn("Scotty", 17214);
+        verifyNoInteractions(promoService);
     }
 }
--- src/test/java/AndrewWebServices/InMemoryDatabaseTest.java
+++ src/test/java/AndrewWebServices/InMemoryDatabaseTest.java
@@ -5,10 +5,10 @@ public class InMemoryDatabaseTest {
         InMemoryDatabase db = new InMemoryDatabase();
 
         db.setPassword("Scotty", 17214);
-        db.setPassword("Ex10si0n", 12345);
+        db.setPassword("zhixin", 12345);
 
         System.out.println(db.getPassword("Scotty")); // 17214
-        System.out.println(db.getPassword("Ex10si0n")); // 12345
+        System.out.println(db.getPassword("zhixin")); // 12345
         System.out.println(db.getPassword("Petter Griffin")); // 0
     }
 }
